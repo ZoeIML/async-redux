00e1657b04ea670267425675f614bd7a6f5c8999
Sarah McLaughlin <sarahmclaughlin0@gmail.com>
2018-04-26 10:56:57 +1200

    reddit

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,32 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-    Fetch Posts
-  </button>
-)
+class LoadSubreddit extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      subreddit: ''
+    }
+    this.handleOnChange = this.handleOnChange.bind(this)
+  }
+
+  handleOnChange (e) {
+    e.preventDefault()
+    this.setState({
+      subreddit: e.currentTarget.value
+    })
+  }
+  render () {
+    return (
+      <div>
+        <p>Enter your search criteria (without spaces)</p>
+        <input onChange={this.handleOnChange}></input>
+        <button onClick={() => this.props.dispatch(fetchPosts(this.state.subreddit))}>
+          Fetch Posts
+        </button>
+      </div>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
